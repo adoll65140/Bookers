0d5b1fb72de16d594ce0395f95391a0370faf283
adoll65140 <vattirii@gmail.com>
2021-01-11 01:27:04 +0000

    new

---
+++ Bookers/app/controllers/books_controller.rb
@@ -0,0 +1,28 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def edit
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path
+  end
+  
+  
+  private
+  def book_params
+    params.permit(:title, :body)
+  end 
+  
+end
+
