a801a2dba64a49bf32fa010ad0dafa566434bda7
adoll65140 <vattirii@gmail.com>
2021-01-11 02:20:19 +0000

    index

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -1,8 +1,10 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
+    @blog = Book.find(params[:id])
   end
 
   def new
@@ -15,7 +17,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path
+    redirect_to book_path(blog.id)
   end
   
   
